93cdead021ab798fa9eaa7d180a3a20f96b92849
AJ-111 <100746162+AJ-111@users.noreply.github.com>
2022-07-26 12:38:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,20 +3,25 @@ class Stack:
     def __init__(self, size):
         self.items = []
         self.size = size
+        self.top = -1
 
     def is_empty(self):
-        # Write code here
+        if(self.top = -1):
+            print("the stack is empty")
 
     def is_full(self):
-        # Write code here
+        if(self.top = self.size - 1):
+            print("the stack is full")
 
     def push(self, data):
         if not self.is_full():
-            # Write code here
+            self.top += 1
+            self.stack.appened(data)
 
     def pop(self):
         if not self.is_empty():
-            # Write code here
+            self.top -= 1
+            self.stack.pop()
 
     def status(self):
         # Write code here
